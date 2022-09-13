45db7feb11c7365324d6c1bf466ab61225f4e38b
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2022-09-13 14:16:11 +0100

    Adição do card feedback e sugestões

--- src/components/Credit/index.tsx
+++ src/components/Credit/index.tsx
@@ -11,6 +11,10 @@ const WarningWrapper = styled.div`
   justify-content: center;
 `
 
+const Margem = styled.div`
+  margin: 1rem;
+`
+
 const Credit = () => {
   return (
     <PageWrapper>
@@ -18,21 +22,23 @@ const Credit = () => {
         <AutoColumn gap="lg">
           <OutlineCard>
             <TYPE.main>Doe e ajude a suportar o projeto</TYPE.main>
+            <TYPE.main>&nbsp;</TYPE.main>
             <TYPE.main fontSize="12px">Redes: [Polygon, BSC ou Ethereum]</TYPE.main>
             <GreyBadge fontSize="12px">
               <TYPE.main>0x4cb1F59c8ba09fED2BCE70943d2ad5dEc599e7d8</TYPE.main>
             </GreyBadge>
-            <TYPE.main fontSize="12px">
-              GitHub:
-              <a href="https://github.com/diemesleno/upool" target="_blank" rel="noreferrer">
-                Contribua com o projeto
-              </a>
-            </TYPE.main>
+            <TYPE.main>&nbsp;</TYPE.main>
+            <TYPE.main fontSize="12px">GitHub:</TYPE.main>
+            <a href="https://github.com/diemesleno/upool" target="_blank" rel="noreferrer">
+              <TYPE.main fontSize="12px">Contribua com o projeto</TYPE.main>
+            </a>
           </OutlineCard>
         </AutoColumn>
+        <Margem />
         <AutoColumn gap="lg">
           <OutlineCard>
             <TYPE.main>Feedback e Sugestões</TYPE.main>
+            <TYPE.main>&nbsp;</TYPE.main>
             <GreyBadge fontSize="12px">
               <TYPE.main>info@orangehat.wtf</TYPE.main>
             </GreyBadge>
