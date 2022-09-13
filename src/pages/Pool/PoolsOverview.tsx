5c8934bef0ca7e494863480a891a3d4d0199b595
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2022-09-13 14:23:34 +0100

    Remoção das pools favoritas pois só funciona se conectar via wallet e não precisamos disso

--- src/pages/Pool/PoolsOverview.tsx
+++ src/pages/Pool/PoolsOverview.tsx
@@ -3,10 +3,8 @@ import { PageWrapper } from 'pages/styled'
 import { AutoColumn } from 'components/Column'
 import { TYPE } from 'theme'
 import PoolTable from 'components/pools/PoolTable'
-import { useAllPoolData, usePoolDatas } from 'state/pools/hooks'
+import { useAllPoolData } from 'state/pools/hooks'
 import { notEmpty } from 'utils'
-import { useSavedPools } from 'state/user/hooks'
-import { DarkGreyCard } from 'components/Card'
 
 export default function PoolPage() {
   useEffect(() => {
@@ -20,21 +18,9 @@ export default function PoolPage() {
       .map((p) => p.data)
       .filter(notEmpty)
   }, [allPoolData])
-
-  const [savedPools] = useSavedPools()
-  const watchlistPools = usePoolDatas(savedPools)
-
   return (
     <PageWrapper>
       <AutoColumn gap="lg">
-        <TYPE.main>Pools Favoritas</TYPE.main>
-        {watchlistPools.length > 0 ? (
-          <PoolTable poolDatas={watchlistPools} />
-        ) : (
-          <DarkGreyCard>
-            <TYPE.main>Suas pools favoritas irão aparecer aqui...</TYPE.main>
-          </DarkGreyCard>
-        )}
         <TYPE.main>Todas Pools</TYPE.main>
         <PoolTable poolDatas={poolDatas} />
       </AutoColumn>
