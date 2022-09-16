bc38a85911b3230bf3957e983ce9d6a9019a7311
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2022-09-16 16:03:55 +0100

    Remoção de import desnecessário

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, useState, useEffect } from 'react'
-import { Route, Switch, Redirect, useLocation } from 'react-router-dom'
+import { Route, Switch, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 import Header from '../components/Header'
 import URLWarning from '../components/Header/URLWarning'
